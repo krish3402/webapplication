ee751956a04fc813693edcf98d7018fcb1abda57
krish3402 <mk.tala654@gmail.com>
2019-10-06 20:57:09 +0530

    check

---
+++ WebApplication5/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5
+{
+    public class Class1
+    {
+        public string hello()
+        {
+            string s = "hello world";
+            return s;
+        }
+    }
+}
---
+++ WebApplication5/sample.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class sample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Class1 s = new Class1();
+            string msg = s.hello();
+            Label1.Text = msg;
+
+        }
+    }
+}
